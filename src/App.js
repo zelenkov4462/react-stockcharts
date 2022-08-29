5b43253a821d912de815600a5d5e018e12ad8137
Nikita Zelenkov <zelenkov4462@gmail.com>
2022-08-29 13:49:32 +0700

    Complete50%

--- src/App.js
+++ src/App.js
@@ -5,48 +5,69 @@ import displayValuesFor from "react-stockcharts/lib/tooltip/displayValuesFor";
 import { TypeChooser } from "react-stockcharts/lib/helper";
 import Chart from "./Chart";
 import { getData } from "./utils";
+import { type } from "@testing-library/user-event/dist/type";
+import React from "react";
 
-function App() {
-  const [charts, setCharts] = useState(null);
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     axios
-  //       .get("./123.json")
-  //       .then((res) => setCharts(res.data))
-  //       .catch((err) => console.log(err));
-  //     console.log(charts);
-  //   }, 1000);
-  // }, []);
+// function App() {
+//   const [charts, setCharts] = useState(null);
+//   // useEffect(() => {
+//   //   setTimeout(() => {
+//   //     axios
+//   //       .get("./123.json")
+//   //       .then((res) => setCharts(res.data))
+//   //       .catch((err) => console.log(err));
+//   //     console.log(charts);
+//   //   }, 1000);
+//   // }, []);
+//
+//   useEffect(() => {
+//     getData().then((data) => {
+//       setCharts(data);
+//     });
+//   }, []);
+//
+//   // useEffect(() => {
+//   //   console.log(charts);
+//   // }, [charts]);
+//
+//   return (
+//     <div>
+//       {charts != null ? (
+//         <TypeChooser>
+//           {(type) => (
+//             <Chart
+//               type={type}
+//               data={charts}
+//               // width={window.innerWidth}
+//               // width={500}
+//               // ratio={2}
+//             />
+//           )}
+//         </TypeChooser>
+//       ) : (
+//         <div>Loading ...</div>
+//       )}
+//     </div>
+//   );
+// }
 
-  useEffect(() => {
+class App extends React.Component {
+  componentDidMount() {
     getData().then((data) => {
-      setCharts(data);
+      this.setState({ data });
     });
-  }, []);
-
-  // useEffect(() => {
-  //   console.log(charts);
-  // }, [charts]);
-
-  return (
-    <div>
-      {charts != null ? (
-        <TypeChooser>
-          {(type) => (
-            <Chart
-              type={type}
-              data={charts}
-              // width={window.innerWidth}
-              // width={500}
-              ratio={2}
-            />
-          )}
-        </TypeChooser>
-      ) : (
-        <div>Loading ...</div>
-      )}
-    </div>
-  );
+    console.log(this.state);
+  }
+  render() {
+    if (this.state == null) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <TypeChooser>
+        {(type) => <Chart type={type} data={this.state.data} />}
+      </TypeChooser>
+    );
+  }
 }
 
 export default App;
